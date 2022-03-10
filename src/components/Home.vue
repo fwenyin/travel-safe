c80d2d2983053b591c8941d7c2278ef51df9a310
dumbasssoul <isabellafunwenyin@gmail.com>
2022-03-10 15:26:38 +0800

    Added geochart for stringency index

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <h1>ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h1>
+  <h2>COVID-19 Stringency Index</h2>
+  <div>
+    <GChart :data = "stringency"
+      :options = "options"
+      :settings = "{ packages: ['geochart'] }"
+      type = "GeoChart"/>
+  </div>
   <h1>At a Glance</h1>
   <p>
     The COVID-19 situation is constantly evolving and knowing information on
     global news and statistics can help you make better travel decisions.
   </p>
   <h1>Keeping Up With COVID-19 News</h1>
-  <div v-for="item in items" :key="item.id">
+  <div v-for="item in news" :key="item.id">
     <h3>{{ item.title }}</h3>
     <p>{{ item.news_agency + " - " + item.time }}</p>
     <p>{{ item.description }}</p>
@@ -14,27 +21,70 @@
 </template>
 
 <script>
-import firebaseApp from "./firebase.js";
-import { getFirestore } from "firebase/firestore";
-import { collection, getDocs, query, limit } from "firebase/firestore";
+import firebaseApp from "../firebase.js";
+import { collection, getDocs, query, limit, getFirestore } from "firebase/firestore";
+import { GChart } from "vue-google-charts";
+import axios from 'axios'
+
 const db = getFirestore(firebaseApp);
+const getCountryISO2 = require("country-iso-3-to-2");
 
 export default {
+  name: "Home",
+
+  components: {
+    GChart
+  },
+
   data() {
     return {
-      items: [],
+      news: [],
+      stringency: [['Country', 'Stringency Index']],
+      options: { 
+        chart: {},
+        colorAxis: {colors: ['#4374e0', '#e7711c']} // orange to blue
+      }
     };
   },
 
   methods: {
-    async display() {
+    async displayNews() {
       let z = await getDocs(query(collection(db, "News"), limit(5)));
       z.forEach((doc) => {
-        console.log(doc.data());
-        this.items.push(doc.data());
-        console.log(this.items);
+        //console.log(doc.data());
+        this.news.push(doc.data());
       });
     },
+
+    // covid19 API data accurate to one day before current day
+    getDate() {
+      let currentDate = new Date();
+			currentDate.setDate(currentDate.getDate() - 2);
+			return currentDate.toISOString().slice(0, 10);
+    },
+
+    async displayMap() {
+      let url = 'https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/' + this.getDate() + '/' + this.getDate() + '';
+      let received  = await axios.get(url)
+      let data = received.data.data[this.getDate()]
+      //console.log(data)
+      Object.keys(data).forEach((key) => {
+        let countryCode = getCountryISO2(data[key].country_code) // convert a country code ISO 3166-1 Alpha-3 to ISO 3166-1 Alpha-2
+        if (typeof countryCode == 'undefined') {
+          console.log(data[key].country_code)
+          return;
+        }
+        let stringencyIndex = data[key].stringency
+        this.stringency.push([countryCode, stringencyIndex])
+      });
+      console.log(this.stringency)
+    },
+
+    display() {
+      this.displayNews();
+      this.displayMap();
+    }
+
   },
 
   created() {
