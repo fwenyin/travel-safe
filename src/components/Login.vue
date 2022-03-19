66e517d93a4fbea3c78bfc92760e2c31535d8c59
radellng <radellng123@gmail.com>
2022-03-20 01:24:22 +0800

    Edited Login Page
    Sign up function is in Login Page.
    Added Bootstrap for style.
    -npm install bootstrap required

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,56 @@
 <template>
-<h1>ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h1>
-<div class = "login">
-  <h2>Log In</h2>
-  <div id = "firebaseui-auth-container"></div>
-  <p>Don't have an account</p>
-      <router-link to="/signup">Sign Up</router-link>
-      <router-view/>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<!-- <div class="header">   
+   <img style="height:15%;width:15%;margin-top: -70px; " src="https://t4.ftcdn.net/jpg/04/41/71/55/360_F_441715520_tVOReRBE2YdytMuDCrIl7O7vES29Otl9.jpg" />
 </div>
+<h3>ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h3>
+<div class = "login">
+      <h3 style="font-weight:bold; ">Log In</h3>
+
+  <div id = "firebaseui-auth-container"></div>
+    <h5>Don't have an account?   <router-link to="/signup">Sign Up</router-link><router-view/> </h5>
+    <h6><router-link to="/signup">Forgot Password?   </router-link><router-view/> </h6>   
+</div>
+<div class="footer">   
+   
+</div> -->
+
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-8 col-lg-6 col-xl-4">
+        <img style="width:100%;height:100%" :src="require(`../assets/login.png`)">
+      </div>
+      <div class="col-md-9 col-lg-6 col-xl-5 offset-xl-1">
+         <img style= "width:25%;height:25%;" :src="require(`../assets/logo.png`)" />
+         <br/> <br/>
+
+      <h4 style="font-weight:bold; font-family: 'Nunito';">ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h4>
+      <br/> <br/>
+        <div class = "login">
+          <h3 style="font-weight:bold; font-family: 'Nunito';">Sign In / Register to TravelSafe</h3>
+          <div id = "firebaseui-auth-container"></div>
+          <br/>
+          <h5 style="font-family: 'Nunito';">Don't have an account? <br/>Click one of the above options to sign up!  </h5>
+
+          <!-- <div style="width:60%; margin:auto;" class="alert alert-secondary" role="alert">
+ Don't have an account? <br/>Click the above options to sign up.
+</div> -->
+          <!-- <h5 style="font-family: 'Nunito';">Don't have an account?   <router-link to="/signup">Sign Up</router-link><router-view/> </h5> -->
+          <!-- <h6 style="font-family: 'Nunito';"><router-link to="/">Forgot Password?   </router-link><router-view/> </h6>    -->
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+    
+  </div> -->
+</section>
 </template>
 
 <script>
@@ -38,18 +82,51 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+.footer {
+  background:url("../assets/background.jpeg")  repeat-x scroll top transparent; 
+   position:absolute;
+   bottom:0;
+   width:99.5%;
+   height:9%;   /* Height of the footer */
+   overflow-y:hidden;
+   /* background:#6cf; */
+}
+.login {
+  margin-left: auto;
+  margin-right: auto;
+  box-sizing: border-box;
+  border-radius: 15px;
+
+  box-shadow: rgba(74, 79, 97, 0.25) 0px 13px 27px -5px, rgba(74, 79, 97, 0.3) 0px 8px 16px -8px;
+  /* background-color:#c9d1dd; */
+  padding: 20px 35px 35px 35px;
+  width:100%;
+  height: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 #firebaseui-auth-container{
-  margin-top: 50px;
-  margin-bottom: 50px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+
 }
+
+.divider:after,
+.divider:before {
+  content: "";
+  flex: 1;
+  height: 1px;
+  background: #eee;
+}
+.h-custom {
+  height: calc(100% - 73px);
+}
+@media (max-width: 450px) {
+  .h-custom {
+    height: 100%;
+  }
+}
+
 
 </style>
--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h1>
+<!-- <h1>ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h1>
 <div class = "signup">
   <h2>Sign Up</h2>
   <div class = "signupform">
@@ -17,7 +17,68 @@
       <p>Already have an account? <router-link to="/">Log In</router-link></p>
     </form>
   </div>
-</div>
+</div> -->
+
+<head>
+<link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<section class="vh-100">
+  <div class="container-fluid h-custom">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-9 col-lg-6 col-xl-5">
+      <img src="https://t4.ftcdn.net/jpg/04/41/71/55/360_F_441715520_tVOReRBE2YdytMuDCrIl7O7vES29Otl9.jpg" />
+      <h1 style="font-weight:bold; font-family: 'Nunito';">ONE-STOP PORTAL FOR TRAVELLING AMIDST COVID-19</h1>
+
+      </div>
+      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+        <div class = "signup">
+          <br/>
+          <h3 style="font-weight:bold; font-family: 'Nunito';">Sign Up with TravelSafe</h3>
+
+
+          <div class = "signupform">
+            <form @submit.prevent="SignUp" id="signupForm">
+              <!-- <label for="username">Username</label>
+              <input type = "text" id = "username" placeholder="avid_traveller123">
+              <br><br>
+              <label for="email">Email</label>
+              <input type = "text" id = "email" placeholder="adambenjamin@gmail.com">
+              <br><br>
+              <label for="password">Password</label>
+              <input type = "password" id = "password" placeholder="Min. 8 characters">
+              <br><br>
+              <div class = "signUpButton">
+                <button id="signUpButton" type="button" v-on:click="savetofs()">Sign Up</button>
+              </div> -->
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+
+            </form>
+          </div>
+
+
+
+          <h5 style="font-family: 'Nunito';">Already have an account?  <router-link to="/">Login</router-link><router-view/> </h5> 
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </template>
 
 <script>
@@ -54,5 +115,23 @@ export default {
 </script>
 
 <style>
+.signup {
+  margin-left: auto;
+  margin-right: auto;
+  box-sizing: border-box;
+  border-radius: 15px;
 
+  box-shadow: rgba(74, 79, 97, 0.25) 0px 13px 27px -5px, rgba(74, 79, 97, 0.3) 0px 8px 16px -8px;
+  /* background-color:#c9d1dd; */
+  padding: 20px 35px 35px 35px;
+  width:100%;
+  height: 100%;
+  /* text-align: center; */
+}
+
+#signupform{
+  margin-top: 10%;
+  margin-bottom: 10%;
+
+}
 </style>
