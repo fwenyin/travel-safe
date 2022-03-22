69ca781bbdc93c1414a9778ccc902c11500990f7
radellng <radellng123@gmail.com>
2022-03-22 20:57:26 +0800

    Started on NavBar.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../components/Login.vue";
 import Signup from "../components/Signup.vue";
 import Home from "../components/Home.vue";
+import NavBar from "../components/NavBar.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/home",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/navbar",
+    name: "NavBar",
+    component: NavBar,
   },
 ];
 const router = createRouter({
