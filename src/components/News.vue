d6dd6a362f746942249f33e98963d9e483b76206
radellng <radellng123@gmail.com>
2022-03-22 21:49:56 +0800

    Installed Packages

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,29 +1,67 @@
 <template>
-  <h1>Global COVID-19 News</h1>
-  <div>
-    <p>Filter</p>
-    <select v-model="selected">
-      <option disabled value="">Select Destination</option>
-      <option>All</option>
-      <option>Australia</option>
-      <option>Denmark</option>
-      <option>Finland</option>
-      <option>Malaysia</option>
-      <option>South Korea</option>
-      <option>United States</option>
-      <option>United Kingdom</option>
-    </select>
-    <p> {{ selected }} </p>
-    <p>Sort</p>
-    <input type="radio" id="newest" value="newest" v-model="sorted">
-    <label for="newest">Newest</label>
-    <input type="radio" id="oldest" value="oldest" v-model="sorted">
-    <label for="oldest">Oldest</label>
-  </div>
-  <div v-for="item in filter" :key="item.id">
-    <h3>{{ item.title }}</h3>
-    <p>{{ item.news_agency + " - " + item.time }}</p>
-    <p>{{ item.description }}</p>
+  <div class="main">
+    <div class="header">
+      <img class="headerImage" :src="require('@/assets/news.png')" alt="home" />
+      <div
+        class="card text-white bg-black mb-5 justify-content-center"
+        style="height: 70px; width: 500px; position: absolute; margin-top: 14%"
+      >
+        <p class="headerText">Global COVID-19 News</p>
+      </div>
+    </div>
+    <div class="body">
+      <div class="left">
+        <div class="card text-white" style="width: 280px">
+          <h6 class="card-header">Filter</h6>
+          <div class="card-body text-center">
+            <button
+              class="btn btn-secondary dropdown-toggle text-black"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              style="margin-top: 10px; margin-bottom: 10px"
+            >
+              {{ selected }}
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li v-for="option in countries" :key="option">
+                <a class="dropdown-item" @click="selected = option">{{
+                  option
+                }}</a>
+              </li>
+            </ul>
+          </div>
+          <h6 class="card-header">Sort</h6>
+          <div class="card-body text-black">
+            <input type="radio" id="newest" value="newest" v-model="sorted" />
+            <label for="newest" style="margin-left: 12px; padding-top: 10px"
+              >Newest</label
+            >
+            <br />
+            <input type="radio" id="oldest" value="oldest" v-model="sorted" />
+            <label for="oldest" style="margin-left: 12px; padding-top: 15px"
+              >Oldest</label
+            >
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <div
+          class="card text-start mx-auto mb-2 p-2 justify-content-center"
+          v-for="item in filter"
+          :key="item.id"
+        >
+          <h5 style="margin-top: 12px">
+            <b>{{ item.title }}</b>
+          </h5>
+          <p style="font-size: 13px">
+            {{ item.news_agency + " - " + item.time }}
+          </p>
+          <p>{{ item.description }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -32,37 +70,50 @@ import firebaseApp from "./firebase.js";
 import { getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 const db = getFirestore(firebaseApp);
-
 export default {
   data() {
     return {
       items: [],
-      selected: "",
+      selected: "Select destination",
       sorted: "",
+      countries: [
+        "All",
+        "Australia",
+        "Denmark",
+        "Finland",
+        "Malaysia",
+        "South Korea",
+        "United States",
+        "United Kingdom",
+      ],
     };
   },
-
   computed: {
     filter() {
       let z = this.items;
-      if (this.selected === "") {
-        return z.sort(function(a, b) {
-          return Date.parse(b.time) - Date.parse(a.time)
-        })
+      if (this.selected === "Select destination") {
+        if (this.sorted === "oldest") {
+          return z.sort(function (a, b) {
+            return Date.parse(a.time) - Date.parse(b.time);
+          });
+        } else {
+          return z.sort(function (a, b) {
+            return Date.parse(b.time) - Date.parse(a.time);
+          });
+        }
       }
-      z = z.filter(article => article.country === this.selected);
+      z = z.filter((article) => article.country === this.selected);
       if (this.sorted === "oldest") {
-        return z.sort(function(a, b) {
-          return Date.parse(a.time) - Date.parse(b.time)
-        })
+        return z.sort(function (a, b) {
+          return Date.parse(a.time) - Date.parse(b.time);
+        });
       } else {
-        return z.sort(function(a, b) {
-          return Date.parse(b.time) - Date.parse(a.time)
-        })
+        return z.sort(function (a, b) {
+          return Date.parse(b.time) - Date.parse(a.time);
+        });
       }
-    }
+    },
   },
-
   methods: {
     async display() {
       let z = await getDocs(collection(db, "News"));
@@ -72,11 +123,66 @@ export default {
       });
     },
   },
-
   created() {
     this.display();
   },
 };
 </script>
 
-<style></style>
+<style>
+.header {
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 5%;
+}
+.headerText {
+  font-size: 30px;
+  margin-top: 3%;
+  text-align: center;
+}
+.body {
+  display: flex;
+  flex-direction: row;
+}
+.headerImage {
+  width: 100%;
+}
+.left {
+  flex: 1;
+  margin-left: 5%;
+}
+.right {
+  flex: 3;
+  width: 85%;
+  margin-right: 5%;
+}
+.dropdown-menu {
+  width: 200px;
+  background-color: #aec4da !important;
+}
+.btn-secondary {
+  width: 200px;
+  background-color: #8caccb !important;
+}
+.card-header {
+  background-color: #394f73 !important;
+}
+.card-body {
+  background-color: #aec4da;
+}
+input[type="radio"] {
+  border: 1px solid white;
+  padding: 0.5em;
+  -webkit-appearance: none;
+}
+input[type="radio"]:checked {
+  background: url(data:image/gif;base64,R0lGODlhAQABAPAAAP///////yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==)
+    no-repeat center center;
+  background-size: 9px 9px;
+}
+input[type="radio"]:focus {
+  outline-color: transparent;
+}
+</style>
