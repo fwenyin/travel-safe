659dbca7af87579a1de6a8cc3b63b6dfe6572ec4
radellng <radellng123@gmail.com>
2022-03-28 02:09:32 +0800

    Finished with Navbar component, started working on Profile Page and Edit Profile

---
+++ src/components/NavBar.vue
@@ -0,0 +1,110 @@
+<template>
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito"
+      rel="stylesheet"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+      crossorigin="anonymous"
+    />
+  </head>
+  <nav class="navbar navbar-expand-lg">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <img
+          style="width: 5%; height: 5%"
+          :src="require(`../assets/logo.png`)"
+        />
+
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="text-align: center">
+          <!-- <img style= "width:15%;height:15%;" :src="require(`../assets/logo.png`)" /> -->
+          <li class="nav-item">
+            <a class="nav-link" style="padding-left: 50px" href="/home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="padding-left: 50px" href="/news">News</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="padding-left: 50px" href="/aboutus"
+              >About Us</a
+            >
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="padding-left: 50px" href="/aboutus"
+              >Forum</a
+            >
+          </li>
+        </ul>
+      </div>
+      <!-- Collapsible wrapper -->
+
+      <!-- Right elements -->
+      <div class="d-flex align-items-center">
+        <!-- Icon -->
+        <a class="me-5" style="color: aliceblue"> Name </a>
+
+        <!-- Avatar -->
+        <div class="dropdown">
+          <a
+            class="dropdown-toggle d-flex align-items-center hidden-arrow"
+            href="#"
+            id="navbarDropdownMenuAvatar"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <img
+              src="https://bootdey.com/img/Content/avatar/avatar7.png"
+              class="rounded-circle"
+              height="25"
+              alt="Black and White Portrait of a Man"
+              loading="lazy"
+            />
+          </a>
+          <ul
+            class="dropdown-menu dropdown-menu-end"
+            aria-labelledby="navbarDropdownMenuAvatar"
+          >
+            <li>
+              <a class="dropdown-item" href="/profile">Profile</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Right elements -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+</template>
+<style>
+.navbar {
+  background-color: #8caccb;
+}
+
+.navbar .nav-item .nav-link {
+  color: aliceblue;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SearchedCountry from '@/components/SearchedCountry'
 import Login from "../components/Login.vue";
-// import Signup from "../components/Signup.vue";
 import Home from "../components/Home.vue";
+import NavBar from "../components/NavBar.vue";
+import News from "../components/News.vue";
+import Profile from "../views/Profile.vue";
 
 const routes = [
     {
@@ -19,7 +21,22 @@ const routes = [
         path: '/searched-country/:country',
         name: 'Searched Country',
         component: SearchedCountry
-    }
+    },
+    {
+      path: "/navbar",
+      name: "NavBar",
+      component: NavBar,
+    },
+    {
+      path: "/news",
+      name: "News",
+      component: News,
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile,
+    },
   ];
 
 const router = createRouter({
